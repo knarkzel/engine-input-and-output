4d56941dcb06f5652242215d2b806e69fb7f1d72
Knarkzel <knarkzel@gmail.com>
2023-06-29 16:21:42 +0200

    Get output as well

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,12 +1,44 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Deserialize, Debug)]
+struct Input {
+    name: String,
+    age: u32,
+    money: f32,
+}
+
+#[derive(Serialize, Debug)]
+struct Output {
+    name: String,
+    taxes: f32,
+}
+
+#[repr(C)]
+struct Slice {
+    len: usize,
+    ptr: *const u8,
+}
+
 #[no_mangle]
-fn main(input_ptr: u32) {
+fn main(input_ptr: u32) -> Box<Slice> {
     // Read string from memory
     let input = unsafe {
         let len = *(input_ptr as *const u32);
         let bytes = (input_ptr + 4) as *const u8;
         let slice = core::slice::from_raw_parts(bytes, len as usize);
-        core::str::from_utf8_unchecked(slice)
+        rmp_serde::from_slice::<Input>(slice).unwrap()
     };
     
-    println!("{input}");
+    println!("{input:?}");
+
+    // Serialize output
+    let output = Output {
+        name: input.name,
+        taxes: input.age as f32 * input.money * 0.10,
+    };
+    let output_bytes = rmp_serde::to_vec_named(&output).unwrap().leak();
+    Box::new(Slice {
+        len: output_bytes.len(),
+        ptr: output_bytes.as_ptr(),
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use anyhow::Result;
-use wasmer::{Instance, Module, Store, Value};
+use anyhow::{anyhow as error, Result};
+use byteorder::{LittleEndian, ReadBytesExt};
+use serde_json::{json, Value as Json};
+use wasmer::{Instance, MemoryView, Module, Store, Value, ValueType, WasmSlice};
 use wasmer_wasi::WasiState;
+
+fn read<T: ValueType>(view: &MemoryView<'_>, offset: u64, length: u64) -> Result<Vec<T>> {
+    Ok(WasmSlice::new(view, offset, length)?.read_to_vec()?)
+}
 
 struct Engine {
     store: wasmer::Store,
@@ -25,43 +31,59 @@ impl Engine {
         Ok(Self { store, instance })
     }
 
-    fn run(&mut self, input: &str) -> Result<Box<[Value]>> {
+    fn run(&mut self, input: Json) -> Result<Json> {
         // Get function
         let function = self.instance.exports.get_function("main")?;
+        let heap_start = 0x110000;
 
-        // Get heap start
-        let heap_start = match self
-            .instance
-            .exports
-            .get_global("__heap_base")
-            .map(|it| it.get(&mut self.store))
-        {
-            Ok(Value::I32(heap_start)) => heap_start,
-            _ => 0x110000,
-        };
+        // Serialize input into bytes
+        let serialized = rmp_serde::to_vec(&input)?;
+        let input_len = (serialized.len() as u32).to_le_bytes();
+        let input_bytes = [&input_len[..], &serialized].concat();
 
         // Grow memory heap for input
         let memory = self.instance.exports.get_memory("memory")?;
-        let pages = (input.len() / wasmer::WASM_PAGE_SIZE) + 1;
+        let pages = (input_bytes.len() / wasmer::WASM_PAGE_SIZE) + 1;
         memory.grow(&mut self.store, pages as u32)?;
 
         // Write bytes into memory
-        let input_len = (input.len() as u32).to_le_bytes();
-        let bytes = [&input_len, input.as_bytes()].concat();
-        {
-            let view = memory.view(&self.store);
-            view.write(heap_start as u64, &bytes)?;
-        }
+        let view = memory.view(&self.store);
+        view.write(heap_start as u64, &input_bytes)?;
 
         // Pass pointer to start of our string
         let result = function.call(&mut self.store, &[Value::I32(heap_start)])?;
-        Ok(result)
+
+        // Deserialize data from pointer
+        match result.first() {
+            Some(Value::I32(pointer)) => {
+                let view = memory.view(&self.store);
+                let output_len = {
+                    let bytes = read::<u8>(&view, *pointer as u64, 4)?;
+                    bytes.as_slice().read_u32::<LittleEndian>()?
+                };
+                let output_ptr = {
+                    let bytes = read::<u8>(&view, *pointer as u64 + 4, 4)?;
+                    bytes.as_slice().read_u32::<LittleEndian>()?
+                };
+                let output_bytes = read::<u8>(&view, output_ptr as u64, output_len as u64)?;
+                Ok(rmp_serde::from_read(output_bytes.as_slice())?)
+            }
+            _ => Err(error!(
+                "Expected pointer to serialized data, got {result:?}"
+            )),
+        }
     }
 }
 
 fn main() -> Result<()> {
     let file = std::env::args().skip(1).next().expect("No file was passed");
     let mut engine = Engine::new(&file)?;
-    engine.run("This is the input from runtime!")?;
+    let input = json!({
+        "name": "James",
+        "age": 50,
+        "money": 50.60,
+    });
+    let output = engine.run(input)?;
+    println!("Output {output}");
     Ok(())
 }
